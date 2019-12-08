1db0d9c201829aec1fcc5750ac698777ab4e15c6
harshshelar22 <harsh.shelar@spit.ac.in>
2019-12-08 13:01:31 +0530

    Initial Commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { EventsPageComponent } from './pages/events-page/events-page.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path : 'home' , component : HomePageComponent },
+  { path : 'about-us' , component : AboutUsPageComponent },
+  { path : 'events' , component : EventsPageComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { AboutUsPageComponent } from './pages/about-us-page/about-us-page.component';
+import { EventsPageComponent } from './pages/events-page/events-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomePageComponent,
+    AboutUsPageComponent,
+    EventsPageComponent
   ],
   imports: [
     BrowserModule,
