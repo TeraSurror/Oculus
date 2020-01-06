23ae9e1ed36f359b1829bdaa9c97f8f428fb64ab
harshshelar22 <harsh.shelar@spit.ac.in>
2020-01-06 20:31:27 +0530

    Sponsors Added

--- src/app/pages/sponsors/sponsors.component.ts
+++ src/app/pages/sponsors/sponsors.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sponsors',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SponsorsComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router:Router) { }
 
   ngOnInit() {
+  }
+
+  onOpenNav(){
+    this.router.navigate(['navigation']);
   }
 
 }
