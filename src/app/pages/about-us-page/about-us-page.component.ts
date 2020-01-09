a8e9aed0c5512b57f3d13cf050e5ca252a6d197e
Minakshi Kudalkar <42650284+minakshi07@users.noreply.github.com>
2020-01-09 23:46:14 +0530

    Nav menu added in about us

--- src/app/pages/about-us-page/about-us-page.component.ts
+++ src/app/pages/about-us-page/about-us-page.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-about-us-page',
@@ -7,9 +8,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class AboutUsPageComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router : Router) {}
+
 
   ngOnInit() {
   }
+  onOpenNav(){
+    this.router.navigate(['navigation']);
+  }
 
+  toHome(){
+    this.router.navigate(['home']);
+  }
 }
