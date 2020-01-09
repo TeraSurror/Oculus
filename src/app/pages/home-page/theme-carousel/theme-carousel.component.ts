46a119702ff3daacd1a5abdfc3ec6a28b9310689
harshshelar22 <harsh.shelar@spit.ac.in>
2020-01-09 14:43:04 +0530

    Theme C File added

---
+++ src/app/pages/home-page/theme-carousel/theme-carousel.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-theme-carousel',
+  templateUrl: './theme-carousel.component.html',
+  styleUrls: ['./theme-carousel.component.css']
+})
+export class ThemeCarouselComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
